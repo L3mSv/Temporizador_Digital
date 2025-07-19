8ff7b6646f810380a684f3af000e0342a29d7501
Lucas <lucaslemes471@gmail.com>
2025-07-19 14:51:06 -0300

    Adicionando contador60 funcional

--- contador60.vhd
+++ contador60.vhd
@@ -1,7 +1,6 @@
 library ieee ;
     use ieee.std_logic_1164.all ;
-    use ieee.numeric_std.all ;
-	 use ieee.std_logic_unsigned.all;
+    use ieee.std_logic_unsigned.all;
 
 entity contador60 is
   port (
@@ -12,15 +11,18 @@ end contador60;
 
 architecture arq of contador60 is
 signal count: std_logic_vector(5 downto 0);
-signal temp: std_logic; 
 begin
-    process(clock, reset, count)
-		begin
-        if reset = '1' or (count = "111100") then 
-            count <= "000000";
-		 elsif rising_edge(clock) then
-            count <= count + 1;
-			end if;
-		 saida <= count;
-    end process;
+  process(clock, reset)
+	begin
+    if reset = '1' then 
+      count <= "000000";
+	  elsif rising_edge(clock) then
+      if count = "111011" then 
+        count <= "000000";
+      else
+        count <= count + 1;
+		  end if;
+	  end if;
+  end process;
+	saida <= count;
 end arq ;
