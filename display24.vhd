91f7222ed25e96cc5ccd86443af4dc03a55e8229
Lucas <lucaslemes471@gmail.com>
2025-07-19 00:50:41 -0300

    Revert "Adicionando mudancas em contador60.vhd"
    This reverts commit 67dbc542e19d20cce3cc6daba41e9114e93ec693.

---
+++ display24.vhd
@@ -0,0 +1,14 @@
+library ieee ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
+
+entity display24 is
+  port (
+  ) ;
+end display24; 
+
+architecture arq of display24 is
+
+begin
+
+end arq ;
